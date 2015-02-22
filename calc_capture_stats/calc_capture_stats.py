98ae3a1786bef37f5936088321fcefa0a4ec76a8
Bruno Grande <contact@brunogrande.me>
2015-02-21 20:00:52 -0800

    added support for excluded regions

--- calc_capture_stats/calc_capture_stats.py
+++ calc_capture_stats/calc_capture_stats.py
@@ -27,24 +27,43 @@ from collections import OrderedDict
 def main():
 
     # Argument parsing
-    parser = argparse.ArgumentParser(description="Calculate useful stats for capture-based "
-                                     "experiments.")
+    parser = argparse.ArgumentParser(description="Calculate useful stats for "
+                                     "capture-based experiments.")
     parser.add_argument("bam_file", help="Input BAM file (sorted, rmduped and indexed). "
                         "Assumes that the BAM file contains reads of one length.")
-    parser.add_argument("bed_file", help="Input BED intervals file.")
+    parser.add_argument("bed_file", help="BED file listing target intervals.")
     parser.add_argument("--output", help="Output TSV stats file.")
+    parser.add_argument("--exclude", help="BED file listing regions to be excluded. "
+                        "Useful for pooled libraries sharing the same multiplex index.")
     args = parser.parse_args()
 
     # Initialize variables
     inbam = pysam.AlignmentFile(args.bam_file, "rb")
     inbed = cancer_api.files.BedFile.open(args.bed_file)
+    excl_bed = cancer_api.files.BedFile.open(args.exclude) if args.exclude else None
     output = args.output if args.output else inbam.filename + ".capture_stats.tsv"
     stats = OrderedDict()
+
+    # Define helper functions
+    def count_interval_reads(bam_file, interval):
+        """Counts the number of reads in a given interval in a BAM file."""
+        reads_iterator = inbam.fetch(interval.chrom, interval.start_pos, interval.end_pos)
+        num_reads = len([1 for r in reads_iterator])
+        return num_reads
 
     # Calculate overall coverage
     read_length = inbam.head(1).next().query_length
     genome_num_mapped = inbam.mapped
     genome_length = sum(inbam.lengths)
+    if excl_bed:
+        # Correct for excluded regions
+        excl_length = 0
+        excl_num_mapped = 0
+        for interval in excl_bed:
+            excl_length += interval.length
+            excl_num_mapped += count_interval_reads(inbam, interval)
+        genome_num_mapped -= excl_num_mapped
+        genome_length -= excl_length
     genome_cov = read_length * genome_num_mapped / float(genome_length)
     stats["Genome_Coverage"] = round(genome_cov, 2)
 
@@ -53,14 +72,13 @@ def main():
     target_num_mapped = 0
     for interval in inbed:
         target_length += interval.length
-        reads_iterator = inbam.fetch(interval.chrom, interval.start_pos, interval.end_pos)
-        target_num_mapped += len([1 for r in reads_iterator])
+        target_num_mapped += count_interval_reads(inbam, interval)
     target_cov = read_length * target_num_mapped / float(target_length)
     stats["Target_Coverage"] = round(target_cov, 2)
 
     # Calculate percent on-target
     percent_on_target = target_num_mapped / float(genome_num_mapped) * 100
-    stats["Percent_On_Target"] = round(percent_on_target, 2)
+    stats["Percent_Reads_On_Target"] = round(percent_on_target, 2)
 
     # Calculate percent fold enrichment
     percent_fold_enrichment = target_cov / genome_cov * 100
