9747785e4c78df86854a354ccc67c6ffe55c595d
jasgrewal <grewalj23@gmail.com>
2015-03-18 16:29:45 -0700

    Debugged issues with chromosome type in pandas

--- compare_files/mafcompare.py
+++ compare_files/mafcompare.py
@@ -19,6 +19,7 @@
 
 import pandas
 import argparse
+import numpy
 
 def main():
 	"""Run augmentmaf_filter.py
@@ -41,19 +42,21 @@ def main():
 	parser_filter.add_argument('-i', '--input', nargs=1, type=argparse.FileType('r'),required=True, help="Specify the file you want to filter")
 	parser_filter.add_argument('-genes', '--genelist', nargs=1, type=argparse.FileType('r'), required=False, default = ['/home/jgrewal/scripts/ref/genes_lymphoma.txt'],help="Specify the gene list you wish to filter against")
 	parser_filter.add_argument('-out', '--output_file', nargs=1, type=str, required=True, help='Specify the prefix to identify output file.')
-	parser_filter.add_argument('-header', '--header_row', nargs=1, type=int, default=[0], help='If your input file has a differenet header index, pass it here')
+	parser_filter.add_argument('-header', '--header_row', nargs=1, type=int, default=[0], help='If your input file has a different header index, pass it here')
 
     	# Parse command line arguments
 	args = parser.parse_args()
 	if args.subcommand == 'filecompare': 
-		input_maf_1 = pandas.DataFrame.from_csv(args.input[0], sep="\t",index_col=None,header=args.header_row[0])
-		input_maf_2 = pandas.DataFrame.from_csv(args.input[1], sep="\t",index_col=None,header=args.header_row[1])
+		input_maf_1 = pandas.DataFrame.from_csv(args.input[0], sep="\t",index_col=None,header=args.header_row[0]);
+		input_maf_1 = updatecoltypes(input_maf_1);
+		input_maf_2 = pandas.DataFrame.from_csv(args.input[1], sep="\t",index_col=None,header=args.header_row[1]);
+		input_maf_2 = updatecoltypes(input_maf_2);
 		is_merging = args.merge
 	
 		#Get your intersects and unique values
 		maf1only = onlyInLeft(input_maf_1,input_maf_2)
 		maf2only = onlyInLeft(input_maf_2,input_maf_1)
-		intersect = InBoth(input_maf_1,input_maf_2)
+		intersect = InBoth(input_maf_1,maf1only)
 
 		maf1only.to_csv((args.output_file[0]+"_only.maf"), sep="\t", index=False)
 		maf2only.to_csv((args.output_file[1]+"_only.maf"),sep="\t", index=False)
@@ -75,13 +78,28 @@ def onlyInLeft(table1,table2):
         return table1[~((table1.Chromosome.isin(table2.Chromosome)) & (table1.Start_Position.isin(table2.Start_Position)) & (table1.End_Position.isin(table2.End_Position)))]
 
 def InBoth(table1,table2):
-        return table1[((table1.Chromosome.isin(table2.Chromosome)) & (table1.Start_Position.isin(table2.Start_Position)) & (table1.End_Position.isin(table2.End_Position)))]
+# return table1[((table1.Chromosome.isin(table2.Chromosome)) & (table1.Start_Position.isin(table2.Start_Position)) & (table1.End_Position.isin(table2.End_Position)))]
+    tempmerge = pandas.concat([table1,table2]).reset_index(drop=True)
+    return tempmerge.groupby(["Chromosome","End_Position","Start_Position"]).filter(lambda x: len(x) ==1)
 
 def GeneFilter(table1,table2):
 	return table1[((table1.Hugo_Symbol.isin(table2.loc[0:,0])))]
 
 def GeneFilter_exclude(table1,table):
         return table1[~((table1.Hugo_Symbol.isin(table2.loc[0:,0])))]
+
+def updatecoltypes(mytable):
+    temptable=makecolint(mytable,["Start_Position","End_Position"])
+    temptable=makecolstr(temptable,["Chromosome"])
+    return temptable
+
+def makecolint(table1,listofcols):
+        table1[listofcols] = table1[listofcols].astype(numpy.int64)
+        return table1
+
+def makecolstr(table1,listofcols):
+    table1[listofcols] = table1[listofcols].astype(str)
+    return table1
 
 if __name__ == '__main__':
 	main()
