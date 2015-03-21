ca89eecee137f460392e3f2ce4e2dacb2d7dd94c
jasgrewal <grewalj23@gmail.com>
2015-03-20 21:43:01 -0700

    Modified in_seg to only generate bed files. This option no longer plots heatmaps

--- compare_files/genes_in_cn.py
+++ compare_files/genes_in_cn.py
@@ -3,10 +3,10 @@
 #Genelist bed is of format: 	"chr	start	end	genename"
 #Input beds are of format: 	"chr	start	end	copynum"
 #You can also pass in seg files (_segs.txt from Titan) but you must specify an output directory for the bed files that are generated from these as an intermediate step
-
+#Note that using in_seg will only generate your bed files. You must subsequently pass these bed files to this script with in_bed, in order to plot the copy number distribution.
 #Author: Jasleen Grewal
 #Date Created: March 11, 2015
-#Date Modified: March 11, 2015
+#Date Modified: March 20, 2015
 #~~~~~~~~~~~~
 
 import argparse
@@ -33,34 +33,39 @@ def main():
 
 	#Subparser when input is a list of segs
 	parser_files = subparsers.add_parser('in_seg')
-	parser_files.add_argument('-g', '--genebed', nargs=1, type=str,required=True,help='Specify the gene bedfile')
+    #parser_files.add_argument('-g', '--genebed', nargs=1, type=str,required=True,help='Specify the gene bedfile')
 	parser_files.add_argument('-i', '--segfiles', nargs='+', type=str, required=True, help='A space separated list of *_segs.txt files')
 	parser_files.add_argument('-out', '--outdir', nargs=1, type=str, required=True, help='Output directory for the processed bed files')
-        parser_files.add_argument('-oprefix', '--outfile_prefix', nargs=1, type=str, required=False, default=["cn_gene"], help='Specify the prefix for the output files.')
+        #parser_files.add_argument('-oprefix', '--outfile_prefix', nargs=1, type=str, required=False, default=["cn_gene"], help='Specify the prefix for the output files.')
 
 	#Parse command line arguments
 	args = parser.parse_args()
-	input_genelist=args.genebed[0]
-	output_prefix=args.outfile_prefix[0]
-	out_consolidated="".join([output_prefix,"_consolidated.txt"])
-	out_counts="".join([output_prefix,"_counts.txt"])
-	out_image="".join([output_prefix,".png"])
 	input_bedlist=""
 	filecount=0
 
 	if args.subcommand == 'in_bed':
+		input_genelist=args.genebed[0]
+		output_prefix=args.outfile_prefix[0]
+		out_consolidated="".join([output_prefix,"_consolidated.txt"])
+		out_counts="".join([output_prefix,"_counts.txt"])
+		out_image=output_prefix
+
 		filecount = len(args.bedfiles)
 		mybedlist = []
 		for i in range(0,filecount):
 			mybedlist.append(args.bedfiles[i])
-
 		input_bedlist= " ".join(mybedlist)
+		fullcomparsion, countscomparison = getbedintersect(input_genelist, input_bedlist,out_consolidated,out_counts,out_image,filecount)
+		print "The consolidated results are at: ", out_consolidated
+		print "The compact result counts are at: ", out_counts
+		print "View it graphically! : ", "".join([out_image,"_*.png"])
+
 		
 	if args.subcommand == 'in_seg':
 		filecount = len(args.segfiles)
 		outdir = args.outdir[0]
 		if not os.path.exists(outdir):
-			os.makedirs(outdir)
+	            os.makedirs(outdir)
 		outdir=re.sub("\/$","",outdir);
 		mybedlist=[]
 		for i in range(0,filecount):
@@ -71,14 +76,10 @@ def main():
 			mycmd="".join(["cut -f 2-4,10 ",segfile," | sed -e 's/^/hs/' | tail -n +2 > ",outfile])
 			os.system(mycmd)
 			mybedlist.append(outfile)
-
 		input_bedlist= " ".join(mybedlist)
  		print "Your bed files are in: ", outdir
 
-	fullcomparsion, countscomparison = getbedintersect(input_genelist, input_bedlist,out_consolidated,out_counts,out_image,filecount)
-	print "The consolidated results are at: ", out_consolidated
-	print "The compact result counts are at: ", out_counts
-	print "View it graphically! : ", out_image
+
 	
 def getbedintersect(genebed,listofbeds,out1,out2,outimg,numfiles):
 	bedstring= listofbeds #" ".join(listofbeds)
