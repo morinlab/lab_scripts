d4d4d37128f3b07ba47e38262aa677f8efe19780
Prasath <ppararaj@gmail.com>
2015-10-20 01:17:35 -0700

    Changed bam2fq.py to be similar to simple_bam2fq.py. Much faster.

--- bam2fq/bam2fq.py
+++ bam2fq/bam2fq.py
@@ -18,51 +18,53 @@ Inputs
 Outputs
 -FASTQ files for paired reads
 -FASTQ files for unpaired reads
+-Prints total number of reads processed
 
 Known Issues
 -Ignored read shuffling
 
 """
-
-import argparse
+from __future__ import print_function
 import os
-import gzip
+import argparse
 import string
+import subprocess
+import sys
 
 
 def main():
     """Parse BAM file and convert to FASTQ"""
     # Argument parsing
     args = parse_args()
-    chunk_size = args.num_reads
+    chnk_size = args.num_reads
     bam_infile = args.bam[0]
-    unpaired_outdir = args.single_outdir[0]
-    paired_outdir = args.paired_outdir[0]
-    
+    outdir = args.outdir[0]
+
     # Initialize globals
     paired = False
     p_outstring = '@{0}/1\n{1}\n+\n{2}\n@{0}/2\n{3}\n+\n{4}\n'
     up_outstring = '@{0}\n{1}\n+\n{2}\n'
-    bits = [1, 16, 64, 128, 256, 512, 1024, 2048]
-    p_chunk_number = 1
+    bits = [16, 64, 128, 2048]
+
+    chnk_num = 1
     p_read_count = 0
-    up_chunk_number = 1
     up_read_count = 0
+    tot_read_count = 0
     
     # Initalize data structures
     paired_dict = {}
 
-    # Creates output directories if they don't exist
-    if not os.path.exists(unpaired_outdir):
-        os.mkdir(unpaired_outdir)
+    # Create output directory if !exist
+    if not os.path.exists(os.path.abspath(outdir)):
+        os.mkdir(os.path.abspath(outdir))
 
-    if not os.path.exists(paired_outdir):
-        os.mkdir(paired_outdir)
+    outdir = os.path.abspath(outdir)
 
+    gzip_cmd = "gzip > "
     # Open directories for output
-    paired_chunk_file = open_chunk_file(p_chunk_number, paired_outdir)
-    unpaired_chunk_file = open_chunk_file(up_chunk_number, unpaired_outdir)
-    
+    p_sp = spawn_gzip(chnk_num, outdir, gzip_cmd, "paired_")
+    chnk_num += 1
+    up_sp = spawn_gzip(chnk_num, outdir, gzip_cmd, "unpaired_")
     # Look over each line in bam
     for line in bam_infile:
         bam_line = line.split("\t")
@@ -71,75 +73,66 @@ def main():
         seq = bam_line[9]
         quality = bam_line[10]
 
+        # Supplementary alignment check
+        if sam_flag_list[3]:
+			tot_read_count += 1
+			continue
+
         # Reverse complement sequence and reverse quality string
         # if revcomp flag is set
-        if sam_flag_list[1]:
+        if sam_flag_list[0]:
             seq = revcomp(seq)
             quality = quality[::-1]
-
-        # Unpaired read if supplementary flag is set    
-        if sam_flag_list[4] or sam_flag_list[5] or sam_flag_list[6] or sam_flag_list[7]:
-            if up_read_count >= chunk_size:
-                unpaired_chunk_file.close()
-                up_chunk_number += 1
-                up_read_count = 0
-                unpaired_chunk_file = open_chunk_file(up_chunk_number, unpaired_outdir)
-            read = [qname, seq, quality]
-            write_to_chunk(unpaired_chunk_file, read, up_outstring)
-            up_read_count += 1
-            continue
 
         # Add qname as key if it doesn't exist.
         # If it already exists then current read is part of
         # a pair.
-        if qname not in paired_dict.keys():
-            paired_dict[qname] = ([], [])
-        else:
+        if qname in paired_dict:
             paired = True
+        else:
+            paired_dict[qname] = ([], [])
 
         # Add sequence and quality to list corresponding to the
         # read
-        if sam_flag_list[2]:
+        if sam_flag_list[1]:
             sort_reads(paired_dict[qname], 0, seq, quality)
-        elif sam_flag_list[3]:
+        elif sam_flag_list[2]:
             sort_reads(paired_dict[qname], 1, seq, quality)
 
-        if paired:       
-            if p_read_count >= chunk_size:
-                paired_chunk_file.close()
-                p_chunk_number += 1
-                p_read_count = 0
-                paired_chunk_file = open_chunk_file(p_chunk_number, paired_outdir)
-            t = [qname, paired_dict[qname][0][0], paired_dict[qname][0][1],
-                 paired_dict[qname][1][0], paired_dict[qname][1][1]]
-            write_to_chunk(paired_chunk_file, t, p_outstring)
+        if paired:
+            t = check_read_count(p_read_count, chnk_num, outdir,
+                                 chnk_size, p_sp, gzip_cmd, "paired_")
+            p_read_count, chnk_num, p_sp = t
+            val = [qname, paired_dict[qname][0][0], paired_dict[qname][0][1],
+                   paired_dict[qname][1][0], paired_dict[qname][1][1]]
+            p_sp.stdin.write(p_outstring.format(*val))
             p_read_count += 2
+            tot_read_count += 2
             del paired_dict[qname]
             paired = False
     else:
         bam_infile.close()
-        paired_chunk_file.close()
-        create_interval_file(p_chunk_number, paired_outdir, 'paired')
-    
+
     # Output unpaired reads
     for qname in paired_dict.keys():
-        if up_read_count >= chunk_size:
-            unpaired_chunk_file.close()
-            up_chunk_number += 1
-            up_read_count = 0
-            unpaired_chunk_file = open_chunk_file(up_chunk_number, unpaired_outdir)
-        read = [qname]
-        if len(paired_dict[qname][0]):
-            read = read + paired_dict[qname][0]
-        else:
-            read = read + paired_dict[qname][1]
-        write_to_chunk(unpaired_chunk_file, read, up_outstring)
-        up_read_count += 1
-        del paired_dict[qname]
-    else:
-        unpaired_chunk_file.close()
-        create_interval_file(up_chunk_number, unpaired_outdir, 'unpaired')
+        t = check_read_count(up_read_count, chnk_num, outdir,
+                             chnk_size, up_sp, gzip_cmd, "unpaired_")
+        up_read_count , chnk_num, up_sp = t
+        val = [qname]
 
+        if len(paired_dict[qname][0]):
+            val = val + paired_dict[qname][0]
+        else:
+            val = val + paired_dict[qname][1]
+
+        up_sp.stdin.write(up_outstring.format(*val))
+        up_read_count += 1
+        tot_read_count += 1
+        del paired_dict[qname]
+
+    # Write interval file
+    write_interval_file(outdir)
+    print('read_total:' + str(tot_read_count), file=sys.stderr)
 
 def decompose_flag(flag, bits=[2**x for x in range(12)]):
     """Identifies the bits that are set in the SAM flag"""
@@ -160,39 +153,31 @@ def sort_reads(tuple_to_append, list_index, seq, quality):
     tuple_to_append[list_index].append(quality)
 
 
-def open_chunk_file(chunk_number, filepath="."):
-    """Opens new chunk file for writing"""
-    chunk_file = os.path.join(filepath, '{0}.fastq.gz'.format(chunk_number))
-    return gzip.open(chunk_file, 'wb')
+def spawn_gzip(chnk_num, chnk_dir, gzip_cmd, read_type):
+    """Returns gzip process filehandle."""
+    s = os.path.join(chnk_dir, str(read_type) + "chunk" + str(chnk_num) + '.fastq.gz')
+    p = subprocess.Popen(gzip_cmd + s, bufsize=-1, shell=True,
+                         stdin=subprocess.PIPE)
+    return p
 
 
-def write_to_chunk(chunk_file, read_data, outstring):
-    """Writes formatted string to current chunk"""
-    chunk_file.write(outstring.format(*read_data))
-
-
-def check_read_count(chunk_file, read_count, chunk_number, chunk_size,
-                     output_dir):
-    """DEPRECATED"""
-    """Iterates to next chunk if max. reads has been reached"""
-    flushed = False
-    if read_count > chunk_size:
-        chunk_file.flush()
-        chunk_file.close()
-        chunk_number += 1
-        read_count = 0
-        chunk_file = open_chunk_file(chunk_number, output_dir)
-        flushed = True
-    return chunk_file, read_count, chunk_number, flushed
-
-
-def create_interval_file(chunk_number, directory, prefix):
+def write_interval_file(directory):
     """Creates an interval file in the given directory"""
-    interval_filename = os.path.join(directory, prefix + '_interval.txt')
-    outstring = string.join([str(i) + '.fastq.gz' for i in range(1, chunk_number + 1)], '\n')
+    interval_filename = os.path.join(directory, 'interval.txt')
+    outstring = [ str(x) + '\n' for x in os.listdir(directory)
+                 if os.path.isfile(os.path.join(directory, x))]
     interval_file = open(interval_filename, 'w')
-    interval_file.write(outstring)
+    interval_file.write(string.join(outstring, ''))
     interval_file.close()
+
+
+def check_read_count(rc, chnk_num, chnk_dir, chnk_size, sp, cmd, read_type):
+    """Checks if read count limit reached, spawns new gzip process."""
+    if rc >= chnk_size:
+        chnk_num += 1
+        sp = spawn_gzip(chnk_num, chnk_dir, cmd, read_type)
+        rc = 0
+    return [rc, chnk_num, sp]
 
 
 def parse_args():
@@ -205,8 +190,7 @@ def parse_args():
     # Positional arguments
     parser.add_argument('bam', nargs=1, type=argparse.FileType('r'),
                         help='Specify SAM file.')
-    parser.add_argument("paired_outdir", default='.', nargs=1)
-    parser.add_argument("single_outdir", default='.', nargs=1)
+    parser.add_argument("outdir", default='.', nargs=1)
     # Parsing
     args = parser.parse_args()
     return args
@@ -214,4 +198,5 @@ def parse_args():
 
 if __name__ == '__main__':
     import timeit
-    print(timeit.timeit("main()", setup="from __main__ import main", number=1))
+    print(timeit.timeit("main()", setup="from __main__ import main", 
+                        number=1), file=sys.stderr)
