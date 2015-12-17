4bd953fb26e4ade9d04ffed725f6e52990b6abb5
Dan Fornika <dfornika@gmail.com>
2015-12-16 16:03:37 -0800

    Transposed output matrix.

--- amplicon_vaf/maf2matrix.R
+++ amplicon_vaf/maf2matrix.R
@@ -8,7 +8,7 @@ if(is.null(maf$Variant_ID)) {
   maf$Variant_ID <- paste(as.character(maf$Chromosome), ":", as.character(maf$Start_Position), maf$Reference_Allele, ">", maf$Tumor_Seq_Allele2, sep="")
 }
 
-vaf_matrix <- acast(maf, Sample_ID ~ Variant_ID, value.var = "VAF")
+vaf_matrix <- acast(maf, Variant_ID ~ Sample_ID, value.var = "VAF")
 
 write.table(vaf_matrix, quote=F, sep="\t")
 
