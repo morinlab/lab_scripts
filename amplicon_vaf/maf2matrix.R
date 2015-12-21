485e4ed7e3593e449002609a55cf6662637bdf2a
Dan Fornika <dfornika@gmail.com>
2015-12-21 11:59:58 -0800

    Made row-labels optional in output with '-r' flag.

--- amplicon_vaf/maf2matrix.R
+++ amplicon_vaf/maf2matrix.R
@@ -2,6 +2,14 @@ library(reshape2)
 
 args <- commandArgs(trailingOnly = TRUE)
 
+if("-r" %in% args || "--rownames" %in% args){
+  variant_list_filename <- ""
+  include_rownames_in_output <- TRUE
+} else {
+  variant_list_filename <- "variant_list.txt"
+  include_rownames_in_output <- FALSE
+}
+
 maf <- read.csv(file=args[1], sep="\t", header=T, stringsAsFactors = F)
 
 if(is.null(maf$Variant_ID)) {
@@ -10,5 +18,9 @@ if(is.null(maf$Variant_ID)) {
 
 vaf_matrix <- acast(maf, Variant_ID ~ Sample_ID, value.var = "VAF")
 
-write.table(vaf_matrix, quote=F, sep="\t")
+if(variant_list_filename != "") {
+  write.table(row.names(vaf_matrix), file=variant_list_filename, row.names=FALSE, col.names=FALSE, quote=FALSE, sep='\t')
+}
+
+write.table(vaf_matrix, quote=F, row.names=include_rownames_in_output, sep="\t")
 
