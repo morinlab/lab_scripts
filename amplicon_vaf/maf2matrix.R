863e9a62637ec0964b5334759e7a3029524040c3
Dan Fornika <dfornika@gmail.com>
2015-12-22 12:57:09 -0800

    Added flag -c to include count data interspersed with vaf.

--- amplicon_vaf/maf2matrix.R
+++ amplicon_vaf/maf2matrix.R
@@ -10,13 +10,36 @@ if("-r" %in% args || "--rownames" %in% args){
   include_rownames_in_output <- FALSE
 }
 
+if("-c" %in% args || "--counts" %in% args){
+  include_counts_in_output <- TRUE
+} else {
+  include_counts_in_output <- FALSE
+}
+
+weave = function(...) {
+      l = list(...)
+      output_matrix <- matrix(do.call(rbind, l), nrow = nrow(l[[1]]))
+      rownames(output_matrix) <- rownames(l[[1]])
+      coverage_colnames <- paste0(colnames(l[[1]]), rep("_total_coverage", length(colnames(l[[1]]))))
+      output_colnames <- c(rbind(colnames(l[[1]]), coverage_colnames))
+      colnames(output_matrix) <- output_colnames
+      output_matrix
+}
+
 maf <- read.csv(file=args[1], sep="\t", header=T, stringsAsFactors = F)
 
 if(is.null(maf$Variant_ID)) {
   maf$Variant_ID <- paste(as.character(maf$Chromosome), ":", as.character(maf$Start_Position), maf$Reference_Allele, ">", maf$Tumor_Seq_Allele2, sep="")
 }
 
+maf$Total_Count <- maf$Reference_Count + maf$Alternate_Count
+
 vaf_matrix <- acast(maf, Variant_ID ~ Sample_ID, value.var = "VAF")
+
+if (include_counts_in_output) {
+  total_count_matrix <- acast(maf, Variant_ID ~ Sample_ID, value.var = "Total_Count")
+  vaf_matrix <- weave(vaf_matrix, total_count_matrix)
+}
 
 if(variant_list_filename != "") {
   write.table(row.names(vaf_matrix), file=variant_list_filename, row.names=FALSE, col.names=FALSE, quote=FALSE, sep='\t')
